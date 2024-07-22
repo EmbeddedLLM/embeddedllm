d38f8f571ab93e2d562c714c5816016b971cf043
Sze Yu Sim <34510821+szeyu@users.noreply.github.com>
2024-07-22 17:52:53 +0800

    Update ellm_benchmark.py

--- benchmark/ellm_benchmark.py
+++ benchmark/ellm_benchmark.py
@@ -12,57 +12,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
 from embeddedllm import engine
 from embeddedllm import sampling_params
 
-async def benchmark(input_token_length, output_token_length, model_path, model_name, backend):
-    # Create the profile_model_timing directory if it doesn't exist
-    log_dir = "profile_model_timing"
-    os.makedirs(log_dir, exist_ok=True)
-
-    log_file = os.path.join(log_dir, f'profile_model_timing_{model_name}_{input_token_length}_{output_token_length}.log')
-
-    # Add the log file to the logger (it will append if the file already exists)
-    logger.add(log_file, mode='a')
-
-    # need different parameter for cpu and directml
-    if backend == "cpu":
-        model = engine.EmbeddedLLMEngine(model_path, vision=False, device="cpu", backend=backend)
-    elif backend == "ipex":
-        model = engine.EmbeddedLLMEngine(model_path, vision=False, device="xpu", backend=backend)
-    else:
-        model = engine.EmbeddedLLMEngine(model_path, vision=False, device="", backend=backend)
-
-    logger.info(f"Model: {model_name}")
-
-    model.tokenizer.chat_template = "{% for message in messages %}{{  message['content']}}{% endfor %}"  # Override
-
-    prompt_text = """
-
-    """
-    # Define the path to the file
-    file_path = "sampleText.txt"
-
-    # Open the file and read its contents into the variable
-    with open(file_path, 'r') as file:
-        prompt_text = file.read()
-
-    input_tokens = model.tokenizer.encode(prompt_text)[:input_token_length-1]
-    input_text = model.tokenizer.decode(input_tokens)
-    print(input_text)
-    input_tokens = model.tokenizer.encode(input_text)
-    print(len(input_tokens))
-
-    assert input_token_length-1 == len(input_tokens)
-
-    PromptInputs = {
-        "prompt": input_text
-    }
-
-    sampling_params_config = sampling_params.SamplingParams(
-        max_tokens=output_token_length,
-        top_p=0.1,
-        top_k=1,
-        temperature=1,
-        repetition_penalty=0.01,
-    )
+async def benchmark(input_token_length, output_token_length, model, PromptInputs, sampling_params_config):
 
     start = time.perf_counter()
 
@@ -86,10 +36,8 @@ async def benchmark(input_token_length, output_token_length, model_path, model_n
     logger.info(f"Total time taken: {total_time_taken:.2f} seconds")
 
     average_tps = (input_token_length + output_token_length) / total_time_taken
-    logger.info("Average tps: "+ str(average_tps))
+    logger.info("Average tps: " + str(average_tps))
 
-    # Remove the logger to close the log file
-    logger.remove()
 
 def main():
     parser = argparse.ArgumentParser(description="Benchmark EmbeddedLLM models.")
@@ -98,16 +46,67 @@ def main():
     parser.add_argument('--model_path', type=str, required=True, help='Path to the model')
     parser.add_argument('--token_in', type=int, required=True, help='Number of input tokens (max 2048)')
     parser.add_argument('--token_out', type=int, required=True, help='Number of output tokens')
+    parser.add_argument('--loop_count', type=int, required=True, help='Number of loop')
 
     args = parser.parse_args()
 
-    # Cap the input tokens to 2048
-    if args.token_in > 2048:
-        print("Input tokens capped to 2048.")
-        args.token_in = 2048
+    # Create the profile_model_timing directory if it doesn't exist
+    log_dir = "profile_model_timing"
+    os.makedirs(log_dir, exist_ok=True)
 
-    # Run the async function using asyncio.run()
-    asyncio.run(benchmark(args.token_in, args.token_out, args.model_path, args.model_name, args.backend))
+    log_file = os.path.join(log_dir, f'profile_model_timing_{args.model_name}_{args.token_in}_{args.token_out}.log')
+
+    # Add the log file to the logger (it will append if the file already exists)
+    logger.add(log_file, mode='a')
+
+    prompt_text = """
+    """
+    # Define the path to the file
+    file_path = "sampleText.txt"
+
+    # Open the file and read its contents into the variable
+    with open(file_path, 'r') as file:
+        prompt_text = file.read()
+
+    PromptInputs = {
+        "prompt": prompt_text
+    }
+
+    sampling_params_config = sampling_params.SamplingParams(
+        max_tokens=args.token_out,
+        top_p=0.1,
+        top_k=1,
+        temperature=1,
+        repetition_penalty=0.01,
+    )
+
+    logger.info(f"Model: {args.model_name}")
+
+    # need different parameter for cpu and directml
+    if args.backend == "cpu":
+        model = engine.EmbeddedLLMEngine(args.model_path, vision=False, device="cpu", backend=args.backend)
+    elif args.backend == "ipex":
+        model = engine.EmbeddedLLMEngine(args.model_path, vision=False, device="xpu", backend=args.backend)
+    else:
+        model = engine.EmbeddedLLMEngine(args.model_path, vision=False, device="", backend=args.backend)
+
+    model.tokenizer.chat_template = "{% for message in messages %}{{  message['content']}}{% endfor %}"  # Override
+
+    input_tokens = model.tokenizer.encode(prompt_text)[:args.token_in-1]
+    input_text = model.tokenizer.decode(input_tokens)
+    print(input_text)
+    input_tokens = model.tokenizer.encode(input_text)
+    print(len(input_tokens))
+
+    assert args.token_in-1 == len(input_tokens)
+
+    for _ in range(args.loop_count):
+        # Run the async function using asyncio.run()
+        asyncio.run(benchmark(args.token_in, args.token_out, model, PromptInputs, sampling_params_config))
+
+    # Remove the logger to close the log file
+    logger.remove()
+
 
 if __name__ == "__main__":
     main()
